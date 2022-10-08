58bba6aaee7376d74fda0c90b80076217b993125
ehsanullahfaiq <112988247+ehsanullahfaiq@users.noreply.github.com>
2022-10-07 20:02:11 -0400

    Add files via upload

---
+++ String Methods/src/workClass/Stringmethods.java
@@ -0,0 +1,31 @@
+package workClass;
+
+public class Stringmethods {
+	public static void main(String[] args ) {
+
+		String name = "Faiq";
+		
+		int A = name.length();
+		int B = name.indexOf("q");
+		boolean C = name.isEmpty();
+		String D = name.toUpperCase();
+		char F = name.charAt(3);
+		
+	
+		
+		System.out.println(A);
+		System.out.println(B);
+		System.out.println(C);
+		System.out.println(D);
+		System.out.println(F);
+
+		
+		
+		
+		
+		
+		
+
+	}
+   }
+
